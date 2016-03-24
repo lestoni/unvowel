59f488d00ffc93cb8bd31ab7367a52aa7809b368
Martijn Arts <arts.martijn@gmail.com>
2016-03-24 14:18:39 +0100

    Save on bytes using inlining of one-byte values

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-var VOWELS   = /[aeiou]/gi;
-var VOWELS_AND_SPACE = /[aeiou\s]/g;
-
 exports.parse = function parse(string, join){
   if(typeof string !== 'string'){
     throw new TypeError('Expected a string as the first option');
   }
 
-  return string.replace(join ? VOWELS_AND_SPACE : VOWELS, '');
+  return string.replace(join ? /[aeiou\s]/gi : /[aeiou]/gi);
 };
