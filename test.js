717950a33613863095ee5387432bd17342166470
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2016-03-24 17:52:58 +0100

    improve tests
    - simplify code
    - actually compare the result of `unvowel.parse` directly with the expected result, this way more bugs become visible.

--- test.js
+++ test.js
@@ -3,36 +3,34 @@
 var unvowel = require('./');
 var assert  = require('assert');
 
-var WHITE_SPACE = /\s+/g;
-var VOWELS      = /[aeiou]+/gi;
+var hasWhitespace = function (str) {
+  return /\s/.test(str);
+}
 
 describe('unvowel#parse', function() {
   it('should remove vowels from a string', function(){
     var str = 'aeiou';
-    var unvoweled = unvowel.parse(str).split(/\s+/g);
-
-    assert.equal(unvoweled.indexOf(VOWELS), -1);
+    assert.equal(unvowel.parse(str), '');
   });
 
   it('should not join strings after unvoweling by default', function() {
     var str = 'goofy yuki';
     var unvoweled = unvowel.parse(str);
 
-    assert.notEqual(unvoweled.search(WHITE_SPACE), -1);
+    assert(hasWhitespace(unvoweled));
   });
 
   it('should join strings after unvoweling with join option set to true', function() {
     var str = 'goofy yuki';
     var unvoweled = unvowel.parse(str, true);
 
-    assert.equal(unvoweled.search(WHITE_SPACE), -1);
+    assert(!hasWhitespace(unvoweled));
   });
 
   it('should not join strings after unvoweling with join option set to false', function() {
     var str = 'goofy yuki';
     var unvoweled = unvowel.parse(str, false);
 
-    assert.notEqual(unvoweled.search(WHITE_SPACE), -1);
+    assert(hasWhitespace(unvoweled));
   });
-
 });
